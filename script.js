057534b5069f3a9c520c0f1ba533735508dc60ac
Merry Mac Miller <merrymacmiller@Merrys-MacBook-Air.local>
2018-09-20 21:08:17 -0400

    recipes on the way

---
+++ script.js
@@ -0,0 +1,8 @@
+function marinaraSauce {
+    console.log('- Heat a medium-large saucepan over medium heat.');
+    console.log('- Add 2 tspns of oil and 5 carlic cloves, cook until golden, about 2 minutes');
+    console.log('- Add 1/4 cup water, 2 cans of crushed tomatoes, and salt and season with black pepper to taste');
+    console.log('- Cover the pot bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes');
+    console.log('- Stir in 1/4 roughly chopped fresh basil, salt and pepper as needed.');
+}
+marinaraSauce()
